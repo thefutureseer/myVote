3969c3d9a5b44ffa67f52d6d87aea4ef234c9ca9
thefutureseer <danielharrislimos@outlook.com>
2024-07-12 02:14:18 -0700

    add kung fu panda

--- src/data/voteScript.js
+++ src/data/voteScript.js
@@ -9,7 +9,7 @@ const voteJson =
       "name": "RankUs",
       "title": "Click & rank the candidates (no apparent order to get started)",
       "showCommentArea": true,
-      "choices": ["Wonder Woman", "Dr Strange", "Iron Man", "Bruce Lee", "Black Panther","Captain Marvel", "Captain America"],
+      "choices": ["Wonder Woman", "Dr Strange", "Iron Man", "Kung Fu Panda", "Black Panther", "Bruce Lee", "Captain Marvel", "Captain America"],
       "selectToRankEnabled": true,
       "selectToRankEmptyRankedAreaText": "Order your specific choices for ranking"
      }
